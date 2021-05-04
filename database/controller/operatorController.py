66a3e305ec72f9df29b534b8799b42b06aefb105
darkorange <darkorange233@gmail.com>
2021-05-05 06:25:56 +0800

    修复获取story_title的sql

--- database/controller/operatorController.py
+++ database/controller/operatorController.py
@@ -88,7 +88,8 @@ class Operator:
         return self.db.select(sql=sql, fields=['skill_index', 'operator_name'])
 
     def get_all_stories_title(self):
-        return self.db.select('t_operator_stories', sql='SELECT * FROM t_operator_stories GROUP BY story_title')
+        # return self.db.select('t_operator_stories', sql='SELECT * FROM t_operator_stories GROUP BY story_title')
+        return self.db.select(sql='SELECT story_title FROM t_operator_stories GROUP BY story_title', fields=['story_title'])
 
     def find_operator_all_detail(self, operator_id):
         where = Where({
